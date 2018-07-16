d7bae351de167c4898296e1e6af2a848b4976981
Jason Wang <1335235961@qq.com>
2018-07-16 18:10:16 +0800

    set up dataservice

---
+++ DoMeAFavor.DataService/DoMeAFavor.DataService/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoMeAFavor.DataService.Models
+{
+    /// <summary>
+    /// 用户类
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// 学号。
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 用户名。
+        /// </summary>
+        public String Username { get; set; }
+
+        /// <summary>
+        /// 密码。
+        /// </summary>
+        public String Password { get; set; }
+
+
+
+    }
+}
