0d2f0de224231134597091d4920a53efe5476a53
lyf840360987 <840360987@qq.com>
2018-07-17 17:21:18 +0800

    更改登陆界面

--- DoMeAFavor/DoMeAFavor/SignUpPage.xaml.cs
+++ DoMeAFavor/DoMeAFavor/SignUpPage.xaml.cs
@@ -20,25 +20,11 @@ namespace DoMeAFavor
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class LoginPage : Page
+    public sealed partial class SignUpPage : Page
     {
-        public LoginPage()
+        public SignUpPage()
         {
             this.InitializeComponent();
-        }
-        private void MyPWBox1_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            //ReadOnlyTB.Text = MyPWBox1.Password;
-        }
-
-        private void SingUp_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(SignUpPage));
-        }
-
-        private void Login_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
