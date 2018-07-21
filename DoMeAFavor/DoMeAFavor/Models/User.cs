78a1fa1b0bd44f2491d344dc4a83a9167ca1d808
Jason Wang <1335235961@qq.com>
2018-07-21 10:27:29 +0800

    修改一些数据库的小错误

--- DoMeAFavor/DoMeAFavor/Models/User.cs
+++ DoMeAFavor/DoMeAFavor/Models/User.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using  GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight;
 namespace DoMeAFavor.Models
 {
     /// <summary>
     /// 用户类
     /// </summary>
-    public class User:ObservableObject
+    public class User: ObservableObject
     {
         /// <summary>
         /// 主键
         /// </summary>
-        public int UserId { get; set; }
+        public int _userId;
+
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int UserId
+        {
+            get => _userId;
+            set => Set(nameof(UserId), ref _userId, value);
+        }
 
         /// <summary>
         /// 用户名
@@ -30,22 +39,83 @@ namespace DoMeAFavor.Models
             set => Set(nameof(UserName), ref _userName, value);
         }
 
+        /// <summary>
+        /// 密码。
+        /// </summary>
+        public string _password;
+
+        /// <summary>
+        /// 密码。
+        /// </summary>
         public string PassWord
         {
-            get;
-            set;
+            get =>_password;
+            set =>Set(nameof(PassWord),ref _password,value);
         }
 
-        public string Avatar
-        {
-            get;
-            set;
+        /// <summary>
+        /// 真实姓名。
+        /// </summary>
+        public string _realName;
+
+        /// <summary>
+        /// 真实姓名。
+        /// </summary>
+        public string RealName {
+            get => _realName;
+            set => Set(nameof(RealName), ref _realName, value);
         }
 
-        public DateTime BirthDay
-        {
-            get;
-            set;
+        public int _phoneNumber;
+
+        /// <summary>
+        /// 手机号。
+        /// </summary>
+        public int PhoneNumber {
+            get => _phoneNumber;
+            set => Set(nameof(PhoneNumber), ref _phoneNumber, value);
         }
+
+        public string _major;
+
+        /// <summary>
+        /// 专业。
+        /// </summary>
+        public string Major {
+            get => _major;
+            set => Set(nameof(Major), ref _major, value);
+        }
+
+        public int _class;
+
+        /// <summary>
+        /// 班级。
+        /// </summary>
+        public int Class {
+            get => _class;
+            set => Set(nameof(Class), ref _class, value);
+        }
+
+        public int _points;
+
+        /// <summary>
+        /// 积分。
+        /// </summary>
+        public int Points {
+            get => _points;
+            set => Set(nameof(Points), ref _points, value);
+        }
+
+        public string _avatar;
+
+        /// <summary>
+        /// 头像。
+        /// </summary>
+        public string Avatar {
+            get => _avatar;
+            set => Set(nameof(Avatar), ref _avatar, value);
+        }
+
+
     }
 }
