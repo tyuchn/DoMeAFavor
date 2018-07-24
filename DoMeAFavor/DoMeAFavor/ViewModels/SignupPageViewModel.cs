9539dbd959868af5278b0ec7a47a0708b717c41b
lyf840360987 <840360987@qq.com>
2018-07-24 16:08:02 +0800

    注册判断

--- DoMeAFavor/DoMeAFavor/ViewModels/SignupPageViewModel.cs
+++ DoMeAFavor/DoMeAFavor/ViewModels/SignupPageViewModel.cs
@@ -10,6 +10,7 @@ using DoMeAFavor.Services;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
 
 namespace DoMeAFavor.ViewModels
 {
@@ -41,8 +42,51 @@ namespace DoMeAFavor.ViewModels
         public RelayCommand SignupCommand =>
             _signupCommand ?? (_signupCommand = new RelayCommand(async () =>
             {
-
-                await _userService.AddAsync(user);
+                
+                if(user.UserId.ToString().Length==8)
+                {
+                    if (user.PassWord.Length<16)
+                    {
+                        if(user.PassWord=="22222")
+                        {
+                            if (user.RealName != null)
+                            {
+                                if (user.PhoneNumber.ToString().Length == 11)
+                                {
+                                    if (user.UserName != null)
+                                    {
+                                        if (user.Major != null)
+                                        {
+                                            if (user.Class.ToString().Length==4)
+                                            { 
+                                                await _userService.AddAsync(user);
+                                                await new MessageDialog("注册成功").ShowAsync();
+                                            }
+                                            else
+                                                await new MessageDialog("请输入正确班级号如：1505").ShowAsync();
+                                        }
+                                        else
+                                            await new MessageDialog("请选择专业").ShowAsync();
+                                    }
+                                    else
+                                        await new MessageDialog("请填写用户昵称").ShowAsync();
+                                }
+                                else
+                                    await new MessageDialog("手机号有误").ShowAsync();
+                            }
+                            else
+                                await new MessageDialog("请填写真实姓名").ShowAsync();
+                        }
+                        else
+                            await new MessageDialog("两密码栏输入不同，请重新输入").ShowAsync();
+                    }
+                    else
+                        await new MessageDialog("密码长度有误").ShowAsync();
+                }
+                else
+                    await new MessageDialog(" 学号位数不对").ShowAsync();
+                
+                
             }));
 
         //构造函数
