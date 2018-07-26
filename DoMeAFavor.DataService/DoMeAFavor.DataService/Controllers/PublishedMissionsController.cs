0b2815b4de80e4ce941285408233e899fe0ef0dd
Jason Wang <1335235961@qq.com>
2018-07-26 21:30:33 +0800

    controller

---
+++ DoMeAFavor.DataService/DoMeAFavor.DataService/Controllers/PublishedMissionsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DoMeAFavor.DataService.Models;
+
+namespace DoMeAFavor.DataService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/PublishedMissions")]
+    public class PublishedMissionsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public PublishedMissionsController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Users?userid= , password=
+        [HttpGet]
+        public async Task<IActionResult> GetUser(string userid, string password)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.Users.SingleOrDefaultAsync(m => m.UserId == userid && m.Password == password);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
