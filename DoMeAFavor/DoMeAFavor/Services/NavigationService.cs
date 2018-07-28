28cb249d025cf5e48b2067f800f31b8136bc4a3e
Jason Wang <1335235961@qq.com>
2018-07-28 14:50:03 +0800

    navigate

--- DoMeAFavor/DoMeAFavor/Services/NavigationService.cs
+++ DoMeAFavor/DoMeAFavor/Services/NavigationService.cs
@@ -2,32 +2,50 @@
 
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 using GalaSoft.MvvmLight.Threading;
 
 namespace DoMeAFavor.Services
 {
     public sealed class NavigationService : INavigationService
     {
+        private Frame _frame;
+
+        public void SetFrame(Frame currentFrame)
+        {
+            _frame = currentFrame;
+        }
+
+
+
         public void Navigate(Type sourcePage)
         {
             DispatcherHelper.Initialize();
             DispatcherHelper.CheckBeginInvokeOnUI(() => 
             { 
-            var frame = (Frame)Window.Current.Content;
-            frame.Navigate(sourcePage);
+            //var frame = (Frame)Window.Current.Content;
+            _frame.Navigate(sourcePage);
             });
-        }
+        } 
 
         public void Navigate(Type sourcePage, object parameter)
         {
-            var frame = (Frame)Window.Current.Content;
+            DispatcherHelper.Initialize();
+            DispatcherHelper.CheckBeginInvokeOnUI(() =>
+            {
+                var frame = (Frame)Window.Current.Content;
             frame.Navigate(sourcePage, parameter);
+            });
         }
 
         public void GoBack()
         {
-            var frame = (Frame)Window.Current.Content;
+            DispatcherHelper.Initialize();
+            DispatcherHelper.CheckBeginInvokeOnUI(() =>
+            {
+                var frame = (Frame)Window.Current.Content;
             frame.GoBack();
+            });
         }
     }
 }
