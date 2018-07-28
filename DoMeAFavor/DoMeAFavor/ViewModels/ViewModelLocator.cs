0b4d21b0d072314e3726935360b99dc12bce3333
Jason Wang <1335235961@qq.com>
2018-07-28 11:00:44 +0800

    viewmodellocator

---
+++ DoMeAFavor/DoMeAFavor/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonServiceLocator;
+using DoMeAFavor.Models;
+using DoMeAFavor.Services;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace DoMeAFavor.ViewModels
+{
+    /// <summary>
+    ///     ViewModel定位器。
+    /// </summary>
+    public class ViewModelLocator
+    {
+
+        /// <summary>
+        ///     ViewModel定位器单件。
+        /// </summary>
+        public static readonly ViewModelLocator Instance =
+            new ViewModelLocator();
+
+        /// <summary>
+        ///     构造函数。
+        /// </summary>
+        private ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);//设置默认的SimpleIOC
+            SimpleIoc.Default.Register<INavigationService, NavigationService>();
+            SimpleIoc.Default.Register<IUserService, UserService>();
+            SimpleIoc.Default.Register<IMissionService, MissionService>();
+            //SimpleIoc.Default.Register<IUserMissionService, UserMissionService>();
+
+            SimpleIoc.Default.Register<LoginPageViewModel>();
+            SimpleIoc.Default.Register<HallPageViewModel>();
+            SimpleIoc.Default.Register<SignupPageViewModel>();
+        }
+
+        /// <summary>
+        ///     获得登录ViewModel。
+        /// </summary>
+        public LoginPageViewModel LoginPageViewModel =>
+            SimpleIoc.Default.GetInstance<LoginPageViewModel>();
+
+        /// <summary>
+        ///     获得任务大厅ViewModel。
+        /// </summary>
+        public HallPageViewModel HallPageViewModel =>
+            SimpleIoc.Default.GetInstance<HallPageViewModel>();
+
+        /// <summary>
+        ///     获得注册ViewModel。
+        /// </summary>
+        public SignupPageViewModel SignupPageViewModel =>
+            SimpleIoc.Default.GetInstance<SignupPageViewModel>();
+
+
+
+
+    }
+}
